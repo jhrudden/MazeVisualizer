bac2e5c2aab739a09da0741120a5d79e4cf33fde
jhrudden <johnhenryrudden@gmail.com>
2020-08-10 00:45:27 -0500

    adjust topbar css

--- src/MazeArtVisualizer/TopBar/TopBar.jsx
+++ src/MazeArtVisualizer/TopBar/TopBar.jsx
@@ -9,10 +9,19 @@ export default class TopBar extends Component {
   render() {
     const { prims, resetGrid } = this.props;
     return (
-      <>
-        <button onClick={() => prims()}>Build Maze</button>
-        <button onClick={() => resetGrid()}>Reset Grid</button>
-      </>
+      <div className="navbar">
+        <div className="m-build-dropdown">
+          <button className="m-build-dropbtn">Build Maze &#x25BC;</button>
+          <div className="m-build-content">
+            <a href="#" onClick={() => prims()}>
+              Prims
+            </a>
+          </div>
+        </div>
+        <button className="navBar-buttons" onClick={() => resetGrid()}>
+          Reset Grid
+        </button>
+      </div>
     );
   }
 }

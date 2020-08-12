98564c7d593bb7a31a9dc040210a4564fe40d653
jhrudden <johnhenryrudden@gmail.com>
2020-08-12 00:27:07 -0500

    finish basic TopBar component

--- src/MazeArtVisualizer/TopBar/TopBar.jsx
+++ src/MazeArtVisualizer/TopBar/TopBar.jsx
@@ -10,17 +10,22 @@ export default class TopBar extends Component {
     const { prims, resetGrid } = this.props;
     return (
       <div className="navbar">
-        <div className="m-build-dropdown">
-          <button className="m-build-dropbtn">Build Maze &#x25BC;</button>
-          <div className="m-build-content">
-            <a href="#" onClick={() => prims()}>
-              Prims
-            </a>
+        <div className="buttons">
+          <button className="navbar-contents" onClick={() => resetGrid()}>
+            Reset Grid
+          </button>
+          <div className=" drop-down ">
+            <button className="drop-down-btn">Build Maze</button>
+            <div className="drop-down-content">
+              <a href="#" onClick={() => prims()}>
+                prims
+              </a>
+            </div>
           </div>
+          <button className="navbar-contents" onClick={() => resetGrid()}>
+            Reset Grid
+          </button>
         </div>
-        <button className="navBar-buttons" onClick={() => resetGrid()}>
-          Reset Grid
-        </button>
       </div>
     );
   }

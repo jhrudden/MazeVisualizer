be919e010ed9229d2bf1d2cc20e6cd7aa42aa4ee
jhrudden <johnhenryrudden@gmail.com>
2020-09-14 23:43:14 -0400

    fix drawing but dfsbuilder still buggy

--- src/Algorithms/Building/DFSBuilder.jsx
+++ src/Algorithms/Building/DFSBuilder.jsx
@@ -1,27 +1,28 @@
 import Stack from "../DataStructures/Stack";
 import { getNeighbors } from "../Utils";
 
-//! need to find a way to have some sort of connection or edge that is added to maze array before it is output
+//! need to randomize the wall picking someway
+//? look at wikipedia for help
 export default function dfsBuilder(graph) {
   const startNodeRow = Math.floor(Math.random() * graph.length);
   const startNodeCol = Math.floor(Math.random() * graph[0].length);
-  var currentNode = graph[startNodeRow][startNodeCol];
+  const startNode = graph[startNodeRow][startNodeCol];
   const worklist = new Stack();
-  worklist.insert(currentNode);
+  worklist.insert([startNode, startNode]);
   const mazeDir = new Map();
   const maze = [];
   while (!worklist.isEmpty()) {
-    currentNode = worklist.remove();
-    if (!mazeDir.has(currentNode)) {
-      const currentNeighbors = getNeighbors(graph, currentNode);
+    const [u, v] = worklist.remove();
+    if (!mazeDir.has(v)) {
+      const currentNeighbors = getNeighbors(graph, v);
       for (var i = 0; i < currentNeighbors.length; i++) {
         const currentNeighbor = currentNeighbors[i];
         if (!mazeDir.has(currentNeighbor)) {
-          worklist.insert(currentNeighbor);
+          worklist.insert([v, currentNeighbor]);
         }
       }
-      maze.push(currentNode);
-      mazeDir.set(currentNode, currentNode);
+      maze.push([u, v]);
+      mazeDir.set(u, u);
     }
   }
   console.log(maze);
